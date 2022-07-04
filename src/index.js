624d7f7c3050cde3ab2f31bf600db8f93b5409e9
Hchan2 <seongchan_@naver.com>
2022-07-04 15:22:44 +0900

    의도적인 에러 수정

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // 팩토리얼 함수 테스트
 function factorial(n) {
-    // if (n < 0) {
-    //     throw new Error('0보다 작은 숫자는 사용할 수 없습니다.');
-    // }
+    if (n < 0) {
+        throw new Error('0보다 작은 숫자는 사용할 수 없습니다.');
+    }
 
     if (n === 0) {
         return 1;
